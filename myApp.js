c51638dc9b1ffff5a398cc24370275ba04d8f00a
christ lambert <yyc@hotmail.de>
2022-08-11 13:29:04 +0200

    6. use of .env to change the style of an Object

--- myApp.js
+++ myApp.js
@@ -1,39 +1,38 @@
 let express = require('express');
 let app = express();
+const router = express.Router();
 
+require('dotenv').config();
 
+//2. Start a Working Express Server
+console.log("Hello World");
 
+//3. Serve an HTML File
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+})
 
+//4. Serve Static Assets
+app.use("/public", express.static(__dirname + '/public'));
 
+// 5. Serve JSON on a Specific Route
+// app.get("/json", function(req, res) {
+//   res.json({
+//     "message": "Hello json"
+//   });
+// });
 
+//6. Use the .env File
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({
+      "message": "HELLO JSON"
+    });
+  } else {
+    res.json({
+      "message": "Hello json"
+    });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = router;
